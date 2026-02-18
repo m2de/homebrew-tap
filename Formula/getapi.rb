06536923c9604a2db8b45ed56060c634b67231e0
Marco Mark <marco@silvanite.com>
2026-02-18 16:02:29 +0000

    getapi 1.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/getapi.rb
+++ Formula/getapi.rb
@@ -1,8 +1,8 @@
 class Getapi < Formula
   desc "Guided, interactive walkthroughs for setting up developer API credentials"
   homepage "https://github.com/m2de/getapi"
-  url "https://github.com/m2de/getapi/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/m2de/getapi/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "56c293f0fb86d169c0074feb65aee4891882917bd9b2adb66af493c2b4b8e45e"
   license "MIT"
 
   depends_on "rust" => :build
